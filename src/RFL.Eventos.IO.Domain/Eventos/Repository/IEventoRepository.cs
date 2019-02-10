132fe7a30e603710473ee7f0074685f6d15f702f
rmoraisgit <rafamorais.br@live.com>
2019-02-10 19:53:03 -0200

    Adicionado contrato de repositorio generico. Adicionado contrato especializado de repositorio da entidade Evento.

---
+++ src/RFL.Eventos.IO.Domain/Eventos/Repository/IEventoRepository.cs
@@ -0,0 +1,11 @@
+﻿using RFL.Eventos.IO.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RFL.Eventos.IO.Domain.Eventos.Repository
+{
+    public interface IEventoRepository : IRepository<Evento>
+    {
+    }
+}
---
+++ src/RFL.Eventos.IO.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using RFL.Eventos.IO.Domain.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace RFL.Eventos.IO.Domain.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity<TEntity>
+    {
+        void Adicionar(TEntity obj);
+        TEntity ObterPorId(Guid id);
+        IEnumerable<TEntity> ObterTodos();
+        void Atualizar(TEntity obj);
+        void Remover(Guid id);
+        IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicate);
+        int SaveChanges();
+    }
+}
